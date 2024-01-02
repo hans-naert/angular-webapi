1e0747a7e198a321fb02169644b12427c17117a0
Hans Naert <hans.naert@vives.be>
2024-01-02 17:05:02 +0100

    Add form for signing in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BooksService } from './Services/books.service';
 import { BookListComponent } from './book-list/book-list.component';
 import { Book } from './Models/book.model';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BookListComponent],
+  imports: [CommonModule, RouterOutlet, BookListComponent, FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-webapi';
   books: Book[]= [];
-
+  SignedIn: boolean = true;
+  
   constructor(private booksService: BooksService) { }
+
+  public signIn(form: any) {
+    console.log(`email :${form.controls.email.value})`);
+    console.log(`password :${form.controls.password.value})`);
+    console.log(form);
+  }
 
   ngOnInit() {
     this.booksService.getBooks().subscribe((books) => {
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()],
+  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"fir-project-les","appId":"1:726149397158:web:11fd8ab20a841fe77a9267","storageBucket":"fir-project-les.appspot.com","apiKey":"AIzaSyA9zeI_nv8GUxKuTTMzL_crJD1ujQ-umP0","authDomain":"fir-project-les.firebaseapp.com","messagingSenderId":"726149397158","measurementId":"G-D2CZ16GV39"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"fir-project-les","appId":"1:726149397158:web:344059c454c0642d7a9267","storageBucket":"fir-project-les.appspot.com","apiKey":"AIzaSyA9zeI_nv8GUxKuTTMzL_crJD1ujQ-umP0","authDomain":"fir-project-les.firebaseapp.com","messagingSenderId":"726149397158","measurementId":"G-TFSV085DM2"}))), importProvidersFrom(provideAuth(() => getAuth()))],
 };
